24d221c3ef7882e4ea488b56057bf56a1b42e651
Franco-Segesso <fransege@gmail.com>
2026-06-05 11:09:04 -0300

    ahora chequea si existe alguien con el mismo dni tambien

--- src/main/java/com/grupo6/subastar/repository/PersonaRepository.java
+++ src/main/java/com/grupo6/subastar/repository/PersonaRepository.java
@@ -11,4 +11,7 @@ public interface PersonaRepository extends JpaRepository<Persona, Integer> {
     Optional<Persona> findByEmail(String email);
 
     boolean existsByEmail(String email);
+    
+    // NUEVA LÍNEA: Validar por documento
+    boolean existsByDocumento(String documento);
 }
