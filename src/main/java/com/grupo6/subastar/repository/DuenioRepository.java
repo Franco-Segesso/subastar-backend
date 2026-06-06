56b53050d03143471dac15210c3c7d6d45b19756
Juan Pablo Patalano <patalanogruta@gmail.com>
2026-06-06 14:29:41 -0300

    corregimos errores del home y del catalogo. Recuperamos datos desde el back sobre subasta, item, asistente, duenio y puja para las cards de las subastas.

---
+++ src/main/java/com/grupo6/subastar/repository/DuenioRepository.java
@@ -0,0 +1,7 @@
+package com.grupo6.subastar.repository;
+
+import com.grupo6.subastar.model.Duenio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DuenioRepository extends JpaRepository<Duenio, Integer> {
+}
--- src/main/java/com/grupo6/subastar/repository/PersonaRepository.java
+++ src/main/java/com/grupo6/subastar/repository/PersonaRepository.java
@@ -14,4 +14,6 @@ public interface PersonaRepository extends JpaRepository<Persona, Integer> {
     
     // NUEVA LÍNEA: Validar por documento
     boolean existsByDocumento(String documento);
+
+    
 }
